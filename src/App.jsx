7f4f0eac7e3cf05fa7001fec6667467aa52fa0d6
WahiDestroyer <laamwahidtahosin@gmail.com>
2025-07-01 00:04:27 +0600

    css done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { getDatabase } from "firebase/database";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="flex flex-col items-center justify-center h-screen bg-amber-700 overflow-hidden">
+      <form className="flex items-center gap-5">
+        <input
+          type="text"
+          className="border-2 rounded-2xl bg-amber-500 outline-0 px-2 font-bold"
+        />
+        <button
+          className="capitalize text-lg font-bold bg-green-500 p-1
+         rounded-lg"
+        >
+          add
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </form>
+      <ul className="font-bold text-2xl text-blue-800 capitalize pt-5">
+        <li>hello</li>
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/firebaseConfig.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDBERoBgwmnV-d_QClmkSCazJBnAzZ8r-M",
+  authDomain: "check2-1ee7b.firebaseapp.com",
+  projectId: "check2-1ee7b",
+  storageBucket: "check2-1ee7b.firebasestorage.app",
+  messagingSenderId: "869758851250",
+  appId: "1:869758851250:web:d952f5601a39ab7c7c51ab",
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
