5f8757a947f7f802e0fd8d72e991423c1c5f7f19
WahiDestroyer <laamwahidtahosin@gmail.com>
2025-07-25 19:46:33 +0600

    edit button

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { MdDeleteForever } from "react-icons/md"
 function App() {
   const [fireData, setFireData] = useState("");
   const [fireList, setFireList] = useState([]);
-  const [fireEdit, setFireEdit] = useState(true)
+  const [fireEdit, setFireEdit] = useState(false)
   const db = getDatabase();
 
   const handleSubmit = (e) => {
@@ -20,7 +20,13 @@ function App() {
   };
 
   const handleUpdate = () => {
+
+  }
+
+  const handleUpdateEdit = (item) => {
     setFireEdit(!fireEdit)
+    console.log(item);
+    
   }
 
   useEffect(() => {
@@ -43,14 +49,24 @@ function App() {
           onChange={(e) => setFireData(e.target.value)}
           className="border-2 rounded-2xl bg-amber-500 outline-0 px-2 font-bold"
         />
-        {fireEdit ? 
-          <button
-            className="capitalize text-lg font-bold bg-pink-700 p-1
+        {fireEdit ? (
+          <>
+            <button
+              onClick={handleUpdate}
+              className="capitalize text-lg font-bold bg-pink-700 p-1
          rounded-lg"
-          >
-            Update
-          </button>
-        : 
+            >
+              Update
+            </button>
+            <button
+              onClick={()=>setFireEdit(false)}
+              className="capitalize text-lg font-bold bg-red-600 p-1
+         rounded-lg"
+            >
+              Cancel
+            </button>
+          </>
+        ) : (
           <button
             className="capitalize text-lg font-bold bg-green-500 p-1
          rounded-lg"
@@ -58,14 +74,17 @@ function App() {
           >
             add
           </button>
-        }
+        )}
       </form>
       <ol className="flex flex-col gap-2 font-bold text-2xl text-green-600 capitalize pt-5 h-55 overflow-y-auto no-scrollbar">
         {fireList.map((jinis, i) => (
           <li key={jinis.id} className="flex gap-2">
             <span>{i + 1}. </span>
             <span>{jinis.agun}</span>
-            <button className="text-blue-900" onClick={handleUpdate}>
+            <button
+              className="text-blue-900"
+              onClick={() => handleUpdateEdit(jinis)}
+            >
               <CiEdit />
             </button>
             <button className="text-red-700">
