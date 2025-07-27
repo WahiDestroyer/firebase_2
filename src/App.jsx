3d5c87d6853162e8fc2c864aba40dbdb6dc91a6f
WahiDestroyer <laamwahidtahosin@gmail.com>
2025-07-27 11:31:49 +0600

    update done, error in add

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import { getDatabase, set, ref, push, onValue } from "firebase/database";
+import { getDatabase, set, ref, push, onValue, update } from "firebase/database";
 import { useEffect, useState } from "react";
-import { CiEdit } from "react-icons/ci"
-import { FaAngry } from "react-icons/fa"
-import { MdDeleteForever } from "react-icons/md"
+import { CiEdit } from "react-icons/ci";
+import { FaAngry } from "react-icons/fa";
+import { MdDeleteForever } from "react-icons/md";
 
 function App() {
   const [fireData, setFireData] = useState("");
   const [fireList, setFireList] = useState([]);
-  const [fireEdit, setFireEdit] = useState(false)
+  const [fireEdit, setFireEdit] = useState(false);
+  const [editedData, setEditedData] = useState({
+    agun: "",
+    id: "",
+  });
+
   const db = getDatabase();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!fireData) return alert("kisu lekhak beda 😤" )
+    if (!fireData) return alert("kisu lekhak beda 😤");
     set(push(ref(db, "fire/")), {
       agun: fireData,
     });
@@ -20,21 +25,23 @@ function App() {
   };
 
   const handleUpdate = () => {
-
-  }
+    update(ref(db, "fire/" + editedData.id), {
+      agun: editedData.agun
+    });
+    setFireEdit(false)
+  };
 
   const handleUpdateEdit = (item) => {
-    setFireEdit(!fireEdit)
-    console.log(item);
-    
-  }
+    setFireEdit(!fireEdit);
+    setEditedData(item);
+  };
 
   useEffect(() => {
     onValue(ref(db, "fire/"), (mal) => {
       let malpati = [];
       mal.forEach((jinis) => {
         setFireList(jinis.val());
-        malpati.push({...jinis.val(), id: jinis.key});
+        malpati.push({ ...jinis.val(), id: jinis.key });
       });
       setFireList(malpati);
     });
@@ -45,8 +52,15 @@ function App() {
       <form className="flex items-center gap-5">
         <input
           type="text"
-          value={fireData}
-          onChange={(e) => setFireData(e.target.value)}
+          value={fireEdit ? editedData.agun : fireData}
+          onChange={(e) =>
+            fireEdit
+              ? setEditedData((agardata) => ({
+                  ...agardata,
+                  agun: e.target.value,
+                }))
+              : (e) => setFireData(e.target.value)
+          }
           className="border-2 rounded-2xl bg-amber-500 outline-0 px-2 font-bold"
         />
         {fireEdit ? (
@@ -59,7 +73,7 @@ function App() {
               Update
             </button>
             <button
-              onClick={()=>setFireEdit(false)}
+              onClick={() => setFireEdit(false)}
               className="capitalize text-lg font-bold bg-red-600 p-1
          rounded-lg"
             >
